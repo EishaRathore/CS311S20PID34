b88eecfddf116c4ae2cf7fb2f8b2104d92ca12e8
Manahil Ameer <manahil1054@gmail.com>
2020-07-03 21:06:46 +0500

    button

--- UI/app.js
+++ UI/app.js
@@ -3,4 +3,5 @@ const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
 const header=document.querySelector('.header.container');
 ham.addEventListener('click',()=>{
     ham.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
 });
