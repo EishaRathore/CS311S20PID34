691b41ef26591293908f71874d8a7e06a706f02d
Manahil Ameer <manahil1054@gmail.com>
2020-07-06 18:28:07 +0500

    experiment

--- UI/app.js
+++ UI/app.js
@@ -5,3 +5,19 @@ ham.addEventListener('click',()=>{
     ham.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 });
+var nav = document.getElementById("nav");
+var h1=document.getElementById("h1");
+var btn = document.getElementById("btn");
+
+window.onscroll = function () {
+  if (window.pageYOffset > 100) {
+    nav.style.position = "fixed";
+    nav.style.top = 0;
+    h1.text="";
+    btn.style.top=0;
+   // btn.style.position="fixed";
+  } else {
+    nav.style.position = "absolute";
+    nav.style.top = 20;
+  }
+};
