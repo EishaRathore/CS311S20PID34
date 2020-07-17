18bc4b16847906713f715e05b4db3664df922476
EishaButt <eisharathore5@gmail.com>
2020-07-17 13:02:08 +0500

    add functionlity

--- UI/app.js
+++ UI/app.js
@@ -1,7 +1,8 @@
-const ham=document.querySelector('.header .nav-bar .nav-list .ham');
-const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
-const header=document.querySelector('.header.container');
-ham.addEventListener('click',()=>{
-    ham.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
-});
+
+       const ham=document.querySelector('.header .nav-bar .nav-list .ham');
+       const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+       const header=document.querySelector('.header.container');
+       ham.addEventListener('click',()=>{
+           ham.classList.toggle('active');
+           mobile_menu.classList.toggle('active');
+       }); 
