79748c22aace0125e82255766360ad8f108025ee
Manahil Ameer <manahil1054@gmail.com>
2020-07-19 13:48:53 +0500

    Update Driver.java

--- src/classes/com/za/tutorial/ga/cs/Driver.java
+++ src/classes/com/za/tutorial/ga/cs/Driver.java
@@ -6,6 +6,9 @@
 // package implementation;
 package com.za.tutorial.ga.cs;
 import java.sql.Connection;
+import java.util.ArrayList;
+
+import com.za.tutorial.ga.cs.domain.Schedule;
 
 public class Driver {
      public static final int POPULATION_SIZE=9;
@@ -15,7 +18,9 @@ public class Driver {
      public static final int NUM=1;
      public static final int TOURNAMENT_SELECTION_SIZE=9;
      public static final int NUMB_OF_ELITE_SCHEDULE=9;
+     private int classNumb=1;
      private Data data;
+	private int scheduleNumb=0;
      static String database="";
 static String url="jdbc:mysql://localhost:3306/";
 static String username="root";
@@ -25,31 +30,61 @@ static String username="root";
         
        final Driver driver= new Driver();
        driver.data=new Data();
+       int generationNumber=0;
        driver.printAvailableData();
+       System.out.println(">Generation # "+generationNumber);
+       System.out.println("     Scheduke # |                                    ");
+       System.out.println("Classes [dept,class,room,instructor,Class-Time]      ");
+       System.out.println("                                    | Fitnes | Conflicts");
+       System.out.println("---------------------------------------------------------");  
+       System.out.println("---------------------------------------------------------");
+       GeneticAlgorithm geneticAlgorithm=new GeneticAlgorithm(driver.data);
+       Population population=new Population(Driver.POPULATION_SIZE, driver.data).sortByFitness();
+       population.getSchedule().forEach(schedule-> System.out.println("          "+driver.scheduleNumb++ +
+                                                                "       | "+schedule+"  |  "+
+                                                                String.format("%.5f", schedule.getFitness())+
+                                                                "  | "+schedule.getNumbOfConflicts()));
+    }
+    private void printScheduleAsTable(Schedule schedule,int generation){
+            ArrayList<com.za.tutorial.ga.cs.domain.Class> classes=schedule.getClasses();
+            System.out.println("\n                              ");
+            System.out.println("Class # | Dept | Course (number");
+            System.out.println("                                ");
+            System.out.println("--------------------------------------");
+            System.out.println("------------------------------------------------------------");
+            classes.forEach(x->{
+                    int majorIndex=data.getDept().indexOf(x.getDept());
+                    int coursesIndex=data.getCourse().indexOf(x.getCourse());
+                    int roomsIndex=data.getRooms().indexOf(x.getRoom());
+                    int instructorsIndex=data.getInstructors().indexOf(x.getInstructor());
+                    int classTimeIndex=data.getClassTime().indexOf(x.getClassTime());
+                    System.out.println("                        ");
+                    System.out.println(String.format("  %1$02d", classNumb)+"   | ");    
+                    System.out.println(String.format("%1$4s", data.getDept().get(majorIndex).getName())+"   | ");
+                    System.out.println(String.format("%1$21s",data.getCourse().get(coursesIndex).getName()+" ("+data.getCourse().get(coursesIndex).getNumber()+" , "+
+                    x.getCourse().getmaxNoOfStudent())+")        | ");
+                            
+            });     
     }
         private void printAvailableData(){
         
-        System.out.println("Available Departments=>");
+        System.out.println("Available Departments-->");
         data.getDept().forEach(x-> 
-                System.out.println("name :"+ x.getName() + " capacity :" + x.getCapacity() + " courses :"+ x.getCourses()));
+                System.out.println("name :"+ x.getName() + " courses :"+ x.getCourses()));
         System.out.println("Available Courses=>");
         data.getCourse().forEach(x ->
-                System.out.println("course :"+x.getName() + " id :"+ x.getId()));
+                System.out.println("course# :"+x.getNumber()+", name :"+x.getName() +", max # of students :"+x.getmaxNoOfStudent()+ ",  Instructors :"+x.getInstructors()));
         
         System.out.println("Available Rooms=>");
         data.getRooms().forEach(x ->
-                System.out.println("Room :"+ x.getId() + " capacity :" + x.getSeatingCapacity()));
+                System.out.println("Room # :"+ x.getNumber() + ",   seating capacity :" + x.getSeatingCapacity()));
         System.out.println("Available Instructors=>");
         data.getInstructors().forEach(x ->
-                System.out.println("name :"+ x.getName()+ " id :"+x.getId()));
-        
-        System.out.println("Available instructorFix=>");
-        data.getInstructors().forEach(x->
-                System.out.println("instructor "));
+                System.out.println("ID :"+x.getId()+",  name :"+ x.getName()));
         
         System.out.println("Available MeetingRooms=>");
-        data.getMeetingTime().forEach(x ->
-                System.out.println("id :" + " time:" +x.getName()));
+        data.getClassTime().forEach(x ->
+                System.out.println("id :"+x.getId() + "  time:" +x.getTime()));
         
         System.out.println("---------------------------------------------------------------------------------------------\n");
         
