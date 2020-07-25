f39aaf08efc35e886a6f3d115458f6146c3c5f69
EishaButt <eisharathore5@gmail.com>
2020-07-25 16:43:16 +0500

    edit

--- ActivitySchedulingTool/src/com/za/ga/cs/connectionProvider/dbConnection.java
+++ ActivitySchedulingTool/src/com/za/ga/cs/connectionProvider/dbConnection.java
@@ -3,6 +3,7 @@ import java.sql.*;
 public class dbConnection {
 public static Connection getCon() {
 	try {
+		
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/activityscheduling","root","");
 		return con;
